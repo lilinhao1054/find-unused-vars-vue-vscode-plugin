319337254adc6f2cdd658639a5a63a8602c2bd27
harlowli <harlowli@tencent.com>
2024-07-11 16:10:14 +0800

    fix: remove useless code

--- test/test.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-const fuvv = require('../main');
-
-(async () => {
-    const locArr = await fuvv(fs.readFileSync(path.resolve(__dirname, './cases/export-default.vue')).toString());
-    console.log(locArr);
-})()
